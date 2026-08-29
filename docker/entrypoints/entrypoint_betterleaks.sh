1a471c3df498ac7866a48567a736d4a37c53b7c4
Stefan Fleckenstein <stefan@fleckenstein.co.uk>
2026-08-29 10:03:13 +0100

    feat: betterleaks parser

---
+++ docker/entrypoints/entrypoint_betterleaks.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+set -e
+
+if [[ -z "${GITHUB_WORKSPACE}" ]]; then
+  if [[ -z "${CI_PROJECT_DIR}" ]]; then
+    WORKSPACE=.
+  else
+    WORKSPACE="${CI_PROJECT_DIR}"
+  fi
+else
+  WORKSPACE="${GITHUB_WORKSPACE}"
+fi
+
+export SO_FILE_NAME="${REPORT_NAME}"
+export SO_PARSER_NAME="Betterleaks"
+
+echo ----------------------------------------
+echo Betterleaks
+echo - TARGET:             "$TARGET"
+echo - REPORT_NAME:        "$REPORT_NAME"
+echo - RUN_DIRECTORY:      "$RUN_DIRECTORY"
+if [[ -n "$FURTHER_PARAMETERS" ]]; then
+  echo - FURTHER_PARAMETERS: "$FURTHER_PARAMETERS"
+fi
+
+cd "$RUN_DIRECTORY"
+betterleaks dir $FURTHER_PARAMETERS --no-banner --log-level warn --exit-code 0 --redact --report-format json --report-path "$WORKSPACE/$REPORT_NAME"
+cd "$WORKSPACE"
+
+if [ "$SO_UPLOAD" == "true" ]; then
+  source file_upload_observations.sh
+fi
+
+exit 0
